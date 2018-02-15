43b218b2a5e788c9ea2c03becaf732c8af3b1d74
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2018-02-15 17:42:14 -0200

    php 7.1

---
+++ 7.1/contrib/scripts/pre-start/pre-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PCRE_ENABLE_JIT=${PCRE_ENABLE_JIT:-0} # http://php.net/manual/en/pcre.configuration.php#ini.pcre.jit
+export ZEND_ASSERTIONS=${ZEND_ASSERTIONS:--1} # http://php.net/zend.assertions
+export ASSERT_ACTIVE=${ASSERT_ACTIVE:-On} # http://php.net/assert.exception
+
+for template in /opt/app-root/etc/php.d/*.template; do
+    envsubst < ${template} > $PHP_INI_SCAN_DIR/$(basename ${template} .template)
+done
